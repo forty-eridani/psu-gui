acba60145cbc4a45be22cc183ab1d4c98de8c9c9
forty-eridani <97766806+forty-eridani@users.noreply.github.com>
2026-04-05 11:38:13 -0500

    Working add command

--- gui/src/main.py
+++ gui/src/main.py
@@ -1,3 +1,4 @@
+from os import wait
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QLineEdit, QGridLayout, QScrollArea, QHBoxLayout, QPushButton, QComboBox, QCheckBox
 from PySide6.QtGui import QDoubleValidator
 import pyqtgraph as pg
@@ -8,7 +9,6 @@ from CommandController import Command, CommandController, CommandDictionary
 
 HEIGHT = 600
 WIDTH = 800
-
 EDIT_WIDTH = 400 
 EDIT_HEIGHT = 225 
 
@@ -27,6 +27,13 @@ true_graph_views = {
         Command.UVL_REQ[0]: Command.UVL_REQ,
 }
 
+class CurrentView:
+    def __init__(self, command: tuple[str, int], y_label: str):
+        self.command = command
+        self.y_label = y_label
+
+update_plot = lambda: None
+
 class AddCommand(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -44,8 +51,8 @@ class AddCommand(QMainWindow):
         fields_container = QWidget(container)
         fields_layout = QVBoxLayout(fields_container)
 
-        name_field = QLineEdit()
-        name_field.setPlaceholderText("Command Name")
+        self.name_field = QLineEdit()
+        self.name_field.setPlaceholderText("Command Name")
 
         self.command_field = QComboBox()
         self.command_field.addItems(CommandDictionary.keys()) 
@@ -54,9 +61,9 @@ class AddCommand(QMainWindow):
 
         self.command_field.currentIndexChanged.connect(self.set_hidden)
 
-        time_field = QLineEdit()
-        time_field.setValidator(QDoubleValidator())
-        time_field.setPlaceholderText("Time (s)")
+        self.time_field = QLineEdit()
+        self.time_field.setValidator(QDoubleValidator())
+        self.time_field.setPlaceholderText("Time (s)")
 
         arg_container = QWidget(fields_container)
         arg_layout = QHBoxLayout(arg_container)
@@ -66,13 +73,16 @@ class AddCommand(QMainWindow):
         self.arg_field.setDisabled(True)
 
         self.step = QCheckBox("Step")
+        self.step.setToolTip("If selected, command will generate intermediate commands that step to this command.")
+        self.should_step = self.step.isChecked()
+        self.step.stateChanged.connect(self.set_step)
 
         arg_layout.addWidget(self.arg_field)
         arg_layout.addWidget(self.step)
 
-        fields_layout.addWidget(name_field)
+        fields_layout.addWidget(self.name_field)
         fields_layout.addWidget(self.command_field)
-        fields_layout.addWidget(time_field)
+        fields_layout.addWidget(self.time_field)
         fields_layout.addWidget(arg_container)
 
         self.set_hidden()
@@ -90,7 +100,7 @@ class AddCommand(QMainWindow):
 
         add_button = QPushButton('Add')
         add_button.setStyleSheet("text-align: center")
-        add_button.clicked.connect(self.close)
+        add_button.clicked.connect(lambda: (update_plot(), self.push_command(), self.close()))
 
         bottom_button_layout.addWidget(cancel_button)
         bottom_button_layout.addWidget(add_button)
@@ -111,6 +121,13 @@ class AddCommand(QMainWindow):
         else:
             self.step.show()
 
+    def set_step(self):
+        self.should_step = not self.should_step
+
+    def push_command(self):
+        CommandScheduler.add_command(float(self.time_field.text()), CommandDictionary[self.command_field.currentText()], self.arg_field.text(), self.step.isChecked(), self.name_field.text())
+        update_plot()
+        print("Pushed a command", self.command_field.currentText())
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -131,7 +148,7 @@ class MainWindow(QMainWindow):
 
         self.graph = pg.PlotWidget()
 
-        self.update_graph(Command.PV, "Commanded " + Command.PV[0])
+        self.set_graph(Command.PV, "Commanded " + Command.PV[0])
 
         graph_layout.addWidget(self.graph)
 
@@ -187,7 +204,7 @@ class MainWindow(QMainWindow):
             action = view_menu.addAction("Graph commanded " + name)
 
             # Sometimes I really hate python...
-            action.triggered.connect(lambda *_, cmd=command: self.update_graph(cmd, "Commanded " + cmd[0]))
+            action.triggered.connect(lambda *_, cmd=command: (self.set_graph(cmd, "Commanded " + cmd[0])))
 
         # End of menu section
 
@@ -200,17 +217,18 @@ class MainWindow(QMainWindow):
         layout.addWidget(console_container, 1, 0)
         layout.addWidget(label3, 1, 1)
 
-
-        self.font_metrics = self.output.fontMetrics()
-
         self.w = None
 
     def show_add_window(self):
         if self.w == None:
             self.w = AddCommand()
-            self.w.show()
+        self.w.show()
 
-    def update_graph(self, command_type: tuple[str, int], y_label: str):
+    def set_cur_view(self, commmand: tuple[str, int], y_label: str): 
+        self.current_view = CurrentView(commmand, y_label)
+        print(self.current_view)
+
+    def set_graph(self, command_type: tuple[str, int], y_label: str):
         times, args = CommandScheduler.get_arg_plot(command_type)
         
         self.graph.clear()
@@ -218,6 +236,23 @@ class MainWindow(QMainWindow):
         self.graph.setLabel('left', y_label)
         self.graph.setLabel('bottom', 'Time (s)')
         self.graph.plot(times, args, symbol='+', stepMode="right")
+
+        self.set_cur_view(command_type, y_label)
+
+    def update_graph(self):
+        if (self.current_view != None):
+            times, args = CommandScheduler.get_arg_plot(self.current_view.command)
+            y_label = self.current_view.y_label
+
+            self.graph.clear()
+            self.graph.setTitle(y_label + " Over Time")
+            self.graph.setLabel('left', y_label)
+            self.graph.setLabel('bottom', 'Time (s)')
+            self.graph.plot(times, args, symbol='+', stepMode="right")
+
+            print("Updated cur graph")
+        else:
+            print("No current graph", self.current_view)
 
     def push_cmd(self) -> None:
         full_command = self.line_edit.text() + "\r"
@@ -227,17 +262,10 @@ class MainWindow(QMainWindow):
 
         self.output.setText(self.output.text() + "[DEVICE] " + response)
 
-CommandScheduler.set_step_rate(3)
-
-CommandScheduler.add_command(1.0, Command.PV, "1.0", False, "PV_1")
-CommandScheduler.add_command(2.0, Command.PV, "3.0", False, "PV_1.5")
-CommandScheduler.add_command(10.0, Command.PV, "10.0", True, "PV_2")
-
-# CommandScheduler.run_commands(0.0)
-
 app = QApplication()
 
 window = MainWindow()
+update_plot = window.update_graph
 window.show()
 
 app.exec()
